1915b27d1031747bae41b707124caf85ede85621
Hesham Mosaad <heshamprince616@yahoo.com>
2022-10-18 04:19:26 +0200

    Tut app first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:advanced_project/presentation/resources/langauge_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+
+import 'app/app.dart';
+import 'app/di.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  await initAppModule();
+  runApp(EasyLocalization(
+    supportedLocales: const [ARABIC_LOCAL, ENGLISH_LOCAL],
+    path: ASSET_PATH_LOCALISATIONS,
+    child: Phoenix(child: MyApp()),
+  ));
+}
+
---
+++ lib/presentation/forgot_password/test.dart
@@ -0,0 +1,110 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import '../../app/di.dart';
+import '../common/state_renderer/state_renderer.dart';
+import '../common/state_renderer/state_renderer_impl.dart';
+import '../resources/assets_manager.dart';
+import '../resources/color_manager.dart';
+import '../resources/strings_manager.dart';
+import '../resources/values_manager.dart';
+import 'forgot_password_viewmodel.dart';
+
+class TestForgot extends StatefulWidget {
+  const TestForgot({Key? key}) : super(key: key);
+
+  @override
+  State<TestForgot> createState() => _TestForgotState();
+}
+
+class _TestForgotState extends State<TestForgot> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailTextEditingController = TextEditingController();
+
+  final ForgotPasswordViewModel _viewModel = instance<ForgotPasswordViewModel>();
+
+  _bind() {
+    _viewModel.start(); // tell viewmodel, start ur job
+    _emailTextEditingController.addListener(() => _viewModel.setEmail(_emailTextEditingController.text));
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<FlowState>(
+        stream: _viewModel.outputState,
+        builder: (context, snapshot) {
+          return snapshot.data?.getScreenWidget(context, _getContentWidget(), () => _viewModel.forgotPassword())
+              ?? _getContentWidget();
+        },
+      ),
+    );
+  }
+
+  Widget _getContentWidget() {
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      padding: const EdgeInsets.only(top: AppPadding.p100),
+      color: ColorManager.white,
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const Image(image: AssetImage(ImageAssets.splashLogo)),
+              const SizedBox(height: AppSize.s28),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<bool>(
+                  stream: _viewModel.outputIsEmailValid,
+                  builder: (context, snapshot) {
+                    return TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _emailTextEditingController,
+                      decoration: InputDecoration(
+                          hintText: AppStrings.emailHint.tr(),
+                          labelText: AppStrings.emailHint.tr(),
+                          errorText: (snapshot.data ?? true)
+                              ? null
+                              : AppStrings.invalidEmail.tr()),
+                    );
+                  }
+                ),
+              ),
+              const SizedBox(height: AppSize.s28),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<bool>(
+                  stream: _viewModel.outputIsAllInputValid,
+                  builder: (context, snapshot) {
+                    return SizedBox(
+                      width: double.infinity,
+                      height: AppSize.s40,
+                      child: ElevatedButton(
+                          onPressed: (snapshot.data ?? false)
+                              ? () => _viewModel.forgotPassword()
+                              : () => _viewModel.inputState.add(LoadingState(stateRendererType: StateRendererType.popupLoadingState)),
+                          child: Text(AppStrings.resetPassword.tr())),
+                    );
+                  }
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
